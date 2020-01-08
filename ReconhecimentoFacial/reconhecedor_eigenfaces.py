546a47d636598d4dca956ae2e42969cd3a7b9a15
gabriel <gabrieldossantosvargas@gmail.com>
2020-01-08 09:41:32 -0300

    RecognitionFaceInit

---
+++ ReconhecimentoFacial/reconhecedor_eigenfaces.py
@@ -0,0 +1,33 @@
+import cv2
+
+detectorFace = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+
+reconhecedor = cv2.face.EigenFaceRecognizer_create()
+reconhecedor.read("classificadorEigen.yml")
+largura,altura = 220,220
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+cam = cv2.VideoCapture('C:Users\ROBERTO\Pictures\Pic')
+
+while 1:
+    conectado,imagem = cam.read()
+    imagemCinza = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
+    facesDetectadas = detectorFace.detectMultiScale(imagemCinza,scaleFactor=1.5, minSize=(30,30))
+
+    for (x,y,l,a) in facesDetectadas:
+        imagemFace = cv2.resize(imagemCinza[y:y+a, x:x+l],(largura,altura))
+        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        id,confianca = reconhecedor.predict(imagemFace)
+        nome=''
+        if id == 1 :
+            nome = 'Gabriel'
+        elif id == 2:
+            nome = 'Camilla'
+        elif id==3:
+            nome = 'Joana'
+        cv2.putText(imagem,nome,(x,y+(a+30)),font,2,(0,0,255))
+        cv2.putText(imagem,str(confianca),(x,y + (a+50)),font,2,(0,255,220))
+    cv2.imshow("face" , imagem)
+    cv2.waitKey(15)
+
+cam.release()
+cv2.destroyAllWindows()
---
+++ ReconhecimentoFacial/treinamentoReconhecimentoImagem.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+import numpy as np
+
+eigenface = cv2.face.EigenFaceRecognizer_create(num_components=50,threshold=0)
+fisherface = cv2.face.FisherFaceRecognizer_create()
+lbph = cv2.face.LBPHFaceRecognizer_create()
+
+
+def getImagemComId():
+    caminhos = [os.path.join('img', f) for f in os.listdir('img')]
+
+    faces = []
+    ids = []
+    for caminhoImagem in caminhos:
+        imagemFace = cv2.cvtColor(cv2.imread(caminhoImagem),cv2.COLOR_BGR2GRAY)
+        id = int(os.path.split(caminhoImagem)[-1].split('.')[1])
+        print(id)
+        cv2.imshow("Face",imagemFace)
+        cv2.waitKey(15)
+        faces.append(imagemFace)
+        ids.append(id)
+    return np.array(ids), faces
+
+ids,faces = getImagemComId()
+
+print('Treinando.... ')
+eigenface.train(faces,ids)
+eigenface.write('classificadorEigen.yml')
+
+fisherface.train(faces,ids)
+eigenface.write('classificadorFisher.yml')
+
+lbph.train(faces,ids)
+lbph.write('classificadorLBPH.yml')
+
+
+print('pronto')
